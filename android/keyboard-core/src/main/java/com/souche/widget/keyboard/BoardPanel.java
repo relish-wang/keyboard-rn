e0a14e1e4899c3577933688b43d95c687c4da023
王鑫 <wangxin@souche.com>
2017-09-18 13:51:31 +0800

    feat: init commit

---
+++ android/keyboard-core/src/main/java/com/souche/widget/keyboard/BoardPanel.java
@@ -0,0 +1,20 @@
+package com.souche.widget.keyboard;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+/**
+ * @author Relish Wang
+ * @since 2017/09/18
+ */
+public class BoardPanel extends ViewGroup {
+
+    public BoardPanel(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
